23125680f621ffb168783307767cdc13566c138d
Anshul Sharma <anshulsharma427@gmail.com>
2024-06-03 19:24:03 -0400

    remove secret

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ require('dotenv').config()
 const fileUpload = require('express-fileupload')
 const BlogPost = require('./models/BlogPostSchema')
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DB)
+
+mongoose.connect(process.env.DB).then(() => "Connected to MongoDB");
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(fileUpload())
+app.use(fileUpload({ createParentPath: true }))
 
 app.get("/", (req, res) => {
-    res.render('form')
+    res.render('create')
 })
 
 app.post('/posts/store', async (req, res) => {
-    console.log(req)
     try {
         let image = req.files.image
-        image.mv(path.resolve(__dirname, 'public/img', image.name))
-        const blogpost = await BlogPost.create({
+        await image.mv(path.resolve(__dirname, 'public/img', image.name))
+        await BlogPost.create({
             ...req.body,
             image: '/img/' + image.name
         })
